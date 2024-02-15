e0728511ec3abd4dec0bb4b3be3216ea2a4dd4a4
Calisthetic <vovakalash04@mail.ru>
2024-02-15 17:25:27 +0500

    add middleware

---
+++ Session5/Middleware/CheckCompetitorKeyMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
+
+namespace Session5.Middleware
+{
+    public class CheckCompetitorKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandingMiddleware> _logger;
+        protected readonly IConfiguration _configuration;
+
+        public CheckCompetitorKeyMiddleware(RequestDelegate next, ILogger<ExceptionHandingMiddleware> logger, IConfiguration configuration)
+        {
+            _next = next;
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                if (httpContext.Request.Method != "GET")
+                {
+                    var value = httpContext.Request.Headers["CompetitorKey"];
+                    if (value != _configuration.GetSection("CompetitorKey").Value)
+                    {
+                        throw new Exception("Wrong CompetitoKey value", new Exception("Please check headers at your requests"));
+                    }
+                }
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex, HttpStatusCode.Unauthorized);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex, HttpStatusCode httpStatusCode)
+        {
+            _logger.LogError(ex.InnerException?.Message ?? ex.Message);
+
+            HttpResponse response = context.Response;
+
+            response.ContentType = "application/json";
+            response.StatusCode = (int)httpStatusCode;
+
+            string result = JsonSerializer.Serialize(new
+            {
+                Message = ex.Message,
+                InnerException = ex.InnerException?.Message,
+            });
+
+            await response.WriteAsync(result);
+        }
+    }
+}
---
+++ Session5/Middleware/ExceptionHandingMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
+
+namespace Session5.Middleware
+{
+    public class ExceptionHandingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandingMiddleware> _logger;
+
+        public ExceptionHandingMiddleware(RequestDelegate next, ILogger<ExceptionHandingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (ValidationException ex)
+            {
+                await HandleExceptionAsync(httpContext, ex, HttpStatusCode.BadRequest);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex, HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex, HttpStatusCode httpStatusCode)
+        {
+            _logger.LogError(ex.InnerException?.Message ?? ex.Message);
+
+            HttpResponse response = context.Response;
+
+            response.ContentType = "application/json";
+            response.StatusCode = (int)httpStatusCode;
+
+            string result = JsonSerializer.Serialize(new
+            {
+                Message = ex.Message,
+                InnerException = ex.InnerException?.Message,
+            });
+
+            await response.WriteAsync(result);
+        }
+    }
+}
